9b23e1bc78a3c89755d4e8dd0e00257f2fcb376f
phuhuykoxl <phuhuy02003@gmail.com>
2022-12-11 16:09:07 +0700

    Update web with bootstrap

--- ASSESTS/js1/lienhe.js
+++ ASSESTS/js1/lienhe.js
@@ -1,75 +1,18 @@
-const usernameEle = document.getElementById('username');
-const emailEle = document.getElementById('email');
-const subject = document.getElementById('subject')
-const message = document.getElementById('message')
-
-const btnRegister = document.getElementById('btn-register');
-const inputEles = document.querySelectorAll('.input-row');
-
-btnRegister.addEventListener('click', function () {
-    Array.from(inputEles).map((ele) =>
-        ele.classList.remove('success', 'error')
-    );
-    let isValid = checkValidate();
-
-    if (isValid) {
-        alert('Gửi thành công');
-    }
-});
-
-function checkValidate() {
-    let usernameValue = usernameEle.value;
-    let emailValue = emailEle.value;
-    let subjectValue = subject.value
-    let messageValue = message.value
-
-    let isCheck = true;
-
-    if (usernameValue == '') {
-        setError(usernameEle, 'Tên không được để trống');
-        isCheck = false;
-    } else {
-        setSuccess(usernameEle);
-    }
-    if (messageValue == '') {
-        setError(message, 'Message không đươc');
-        isCheck = false;
-    } else {
-        setSuccess(message);
-    }
-    if (subjectValue == '') {
-        setError(subject, 'Môn học không được để trống');
-        isCheck = false;
-    } else {
-        setSuccess(subject);
-    }
-
-    if (emailValue == '') {
-        setError(emailEle, 'Email không được để trống');
-        isCheck = false;
-    } else if (!isEmail(emailValue)) {
-        setError(emailEle, 'Email không đúng định dạng');
-        isCheck = false;
-    } else {
-        setSuccess(emailEle);
-    }
-
-
-    return isCheck;
-}
-
-function setSuccess(ele) {
-    ele.parentNode.classList.add('success');
-}
-
-function setError(ele, message) {
-    let parentEle = ele.parentNode;
-    parentEle.classList.add('error');
-    parentEle.querySelector('small').innerText = message;
-}
-
-function isEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-    );
-}
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation')
+    Array.from(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          else{
+            alert("Chúng tôi đã nhận được thông tin của bạn, chúng tôi sẽ phản hồi sau 24h");
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })()
+    
